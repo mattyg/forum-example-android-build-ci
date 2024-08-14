9d14897c8bf180a2a868c489c2c54d5f6232d145
Matt Gabrenya <matt@buildyourweb.app>
2024-08-14 10:15:58 -0700

    p2p-shipyard fork making android sdk dir writable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Template for Holochain app development";
 
   inputs = {
-    p2p-shipyard.url = "github:darksoil-studio/p2p-shipyard";
+    p2p-shipyard.url = "github:mattyg/p2p-shipyard?rev=8e58615c889c4a1a58d0a0cdb22055d2e95e97bc";
     versions.url  = "github:holochain/holochain?dir=versions/0_3";
 
     holochain-flake.url = "github:holochain/holochain";
